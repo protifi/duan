739809b7bfdcc6c9256ab286e831c1f6cc0e158b
unknown <kietluong2909@gmail.com>
2021-11-29 18:02:14 +0700

    first commit

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors")
 
-app.get("/", (req,res) =>{
-    res.send("Hello EveryOne")
+
+const bcypt = require('bcrypt')
+
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: `localhost`,
+    user:  `root`,
+    password: ``,
+    database: `mydata`
+});
+app.post("/login",(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query("select * from ds_khachhang where username = ? and password = ?",
+    [username,password],
+    (err,result)=>{
+       if(err){
+           res.send({err:err});
+           res.json();
+       }
+         if(result){
+               res.send(result);
+           }
+        else{
+            res.statusMessage('Login False')
+        }
+    }
+
+    
+    );
 })
 
 app.listen(3001, () =>{
